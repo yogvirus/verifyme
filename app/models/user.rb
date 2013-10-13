05e488a0ec9e731f725f9609587c2eddd0719239
Sudhir Vishwakarma <sudorails@gmail.com>
2013-10-13 11:59:30 +0530

    new commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   attr_accessible :email, :password, :password_confirmation, :remember_me, :role, :tab_id, :name, :profile_attributes           
-  validates_uniqueness_of :tab_id, :allow_nil => true
+  #validates_uniqueness_of :tab_id, :allow_nil => true
   has_one :profile
   belongs_to :tab
   accepts_nested_attributes_for :profile
