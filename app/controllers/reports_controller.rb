a6a12a86669ad05f024c5e96ea1f2fd1f6d32f9a
sudhir <sudorails@gmail.com>
2013-11-25 08:56:49 +0530

    reports ..

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,10 +1,11 @@
 class ReportsController < ApplicationController
 include ApplicationHelper
+include ReportsHelper
   def index
    @start_date = params[:start_date] || Date.today-5
    @end_date = params[:end_date] || Date.today
    if params[:start_date] && params[:end_date]
-     @customers = Customer.where("created_at >= ? and created_at <= ?", @start_date, @end_date)
+     @customers = all_reports(@start_date, @end_date)
     respond_to do |format|
       format.html
       format.xls { render xlsx: :index, filename: "my_items_doc" }
--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,2 +1,9 @@
 module ReportsHelper
+ def all_reports(start_date, end_date)
+   customers = Customer.where("created_at >= ? and created_at <= ?", start_date, end_date)
+   co_applicants = CoApplicant.where("created_at >= ? and created_at <= ?", start_date, end_date)
+   business = Business.where("created_at >= ? and created_at <= ?", start_date, end_date)
+   co_app_business = CoApplicantBusiness.where("created_at >= ? and created_at <= ?", start_date, end_date)
+   reports = customers+co_applicants+business+co_app_business
+ end
 end
