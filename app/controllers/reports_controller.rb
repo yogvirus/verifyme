c51af1e66c5fe472bcf04ba13a538fee3e76b9d4
Sudhir Vishwakarma <sudhirsb2003@gmail.com>
2014-01-28 20:22:51 +0530

    update reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -9,13 +9,10 @@ include ReportsHelper
 
    if @tab_id!=0 && @status!='All' && params[:start_date] && params[:end_date]
      @customers = Servey.joins(:customer).where('serveys.tab_id = ? and serveys.created_at >= ? and serveys.created_at <= ? and customers.status = ? ', @tab_id , Date.today-60, Date.today, @status)
-
    elsif @tab_id!=0 && params[:status]=='All' && params[:start_date] && params[:end_date]
       @customers = Servey.where("tab_id = ? and created_at >= ? and created_at <= ?", @tab_id, @start_date, @end_date)
-
    elsif @tab_id==0 && @status!='All'
      @customers = Servey.joins(:customer).where('serveys.created_at >= ? and serveys.created_at <= ? and customers.status = ? ', Date.today-60, Date.today, @status)
-
    else
      @customers = Servey.where("created_at >= ? and created_at <= ?", @start_date, @end_date)
    end
