02ebafa9a38b247dd287ce0cc0a57e4a3e1ace52
sudhirsb2003 <sudhirsb2003@gmail.com>
2015-01-10 17:44:44 +0530

    few changes in the script(unicorn/ nginx)

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,24 +1,8 @@
-# Set the working application directory
-# working_directory "/path/to/your/app"
-working_directory "/home/deployer/apps/e_verification"
-
-# Unicorn PID file location
-# pid "/path/to/pids/unicorn.pid"
-pid "/home/deployer/apps/e_verification/pids/unicorn.pid"
-
-# Path to logs
-# stderr_path "/path/to/log/unicorn.log"
-# stdout_path "/path/to/log/unicorn.log"
-stderr_path "/home/deployer/apps/e_verification/log/unicorn.log"
-stdout_path "/home/deployer/apps/e_verification/log/unicorn.log"
-
-# Unicorn socket
-listen "/tmp/unicorn.[app name].sock"
+root = "/home/deployer/apps/e_verification/current"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
 listen "/tmp/unicorn.e_verification.sock"
-
-# Number of processes
-# worker_processes 4
 worker_processes 2
-
-# Time-out
 timeout 30
